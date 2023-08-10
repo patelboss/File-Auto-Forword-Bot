2e89b6d8d5c5f8da100f755bb75980c78d9eaa9f
patelboss <83952959+patelboss@users.noreply.github.com>
2023-08-11 00:20:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,42 +1,45 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# (c) Dark Angel
+import logging
+import logging.config
+from pyrogram import Client 
+from config import API_ID, API_HASH, BOT_TOKEN, PORT
+from aiohttp import web
+from plugins.web_support import web_server
 
-from pyrogram import Client, __version__
-
-from config import Config
-from config import LOGGER
-
-from user import User
-
+logging.config.fileConfig('logging.conf')
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
 
 
 class Bot(Client):
-    USER: User = None
-    USER_ID: int = None
 
     def __init__(self):
         super().__init__(
-            Config.BOT_SESSION,
-            api_hash=Config.API_HASH,
-            api_id=Config.API_ID,
-            plugins={
-                "root": "plugins"
-            },
-            workers=4,
-            bot_token=Config.BOT_TOKEN
+            name="imdb",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            workers=50,
+            plugins={"root": "plugins"},
+            sleep_threshold=5,
         )
-        self.LOGGER = LOGGER
 
     async def start(self):
-        await super().start()
-        usr_bot_me = await self.get_me()
-        self.set_parse_mode("html")
-        self.LOGGER(__name__).info(
-            f"@{usr_bot_me.username}  started! "
-        )
-        self.USER, self.USER_ID = await User().start()
+       await super().start()
+       me = await self.get_me()
+       self.mention = me.mention
+       self.username = me.username 
+    
+        
+        app = web.AppRunner(await web_server())
+       await app.setup()
+       bind_address = "0.0.0.0"
+       await web.TCPSite(app, bind_address, PORT).start()
+       logging.info(f"{me.first_name} ✅✅ BOT started successfully ✅✅")
+      
 
     async def stop(self, *args):
-        await super().stop()
-        self.LOGGER(__name__).info("Bot stopped. Bye.")
+      await super().stop()      
+      logging.info("Bot Stopped 🙄")
+        
+bot = Bot()
+bot.run()
