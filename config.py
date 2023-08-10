09ce4bf112498c16d16739674efcfd47ab8ce381
patelboss <83952959+patelboss@users.noreply.github.com>
2023-08-11 02:43:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,32 +1,33 @@
 import os
 import logging
 
+class Config:
 
-API_HASH = os.environ.get("API_HASH", "")
+      API_HASH = os.environ.get("API_HASH", "")
 
-API_ID = os.environ.get("API_ID", "")
+      API_ID = os.environ.get("API_ID", "")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+      BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
-BOT_SESSION = os.environ.get("BOT_SESSION", "bot")
+      BOT_SESSION = os.environ.get("BOT_SESSION", "bot")
 
-CAPTION = os.environ.get("CAPTION", "")
+      CAPTION = os.environ.get("CAPTION", "")
 
-FROM_CHANNEL = os.environ.get("FROM_CHANNEL", None)
+      FROM_CHANNEL = os.environ.get("FROM_CHANNEL", None)
 
-FILTER_TYPE = os.environ.get("FILTER_TYPE", "")
+      FILTER_TYPE = os.environ.get("FILTER_TYPE", "")
 
-OWNER_ID = os.environ.get("OWNER_ID", "")
+      OWNER_ID = os.environ.get("OWNER_ID", "")
 
-LIMIT = os.environ.get("LIMIT", "2500000")
+      LIMIT = os.environ.get("LIMIT", "2500000")
 
-SKIP_NO = os.environ.get("SKIP_NO", "0")
+      SKIP_NO = os.environ.get("SKIP_NO", "0")
 
-SESSION = os.environ.get("SESSION")
+      SESSION = os.environ.get("SESSION")
 
-TO_CHANNEL = os.environ.get("TO_CHANNEL", "")
+      TO_CHANNEL = os.environ.get("TO_CHANNEL", "")
 
-PORT = os.environ.get("PORT", "8080")
+      PORT = os.environ.get("PORT", "8080")
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
